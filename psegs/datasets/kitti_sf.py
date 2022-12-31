9afca1c6a2995cfebf84e8557444b3a4e46e1bdb
Paul Wais <paulwais@gmail.com>
2022-12-30 22:54:07 -0800

    save

--- psegs/datasets/kitti_sf.py
+++ psegs/datasets/kitti_sf.py
@@ -14,6 +14,7 @@
 
 from pathlib import Path
 
+import numpy as np
 from oarphpy import util as oputil
 
 from psegs import util
@@ -109,9 +110,9 @@ def kittisf15_load_flow(path):
   w, h = flow_direct[3]['size']
   flow = np.zeros((h, w, 3), dtype=np.float64)
   for i in range(len(flow_data)):
-      flow[i, :, 0] = flow_data[i][0::3]
-      flow[i, :, 1] = flow_data[i][1::3]
-      flow[i, :, 2] = flow_data[i][2::3]
+    flow[i, :, 0] = flow_data[i][0::3]
+    flow[i, :, 1] = flow_data[i][1::3]
+    flow[i, :, 2] = flow_data[i][2::3]
 
   invalid_idx = (flow[:, :, 2] == 0)
   flow[:, :, 0:2] = (flow[:, :, 0:2] - 2 ** 15) / 64.0
@@ -119,128 +120,158 @@ def kittisf15_load_flow(path):
   flow[invalid_idx, 1] = 0
   return flow[:, :, :2]
 
-def kittisf15_load_disp(disp_path):
-    import imageio
-    
-    # From KITTI SF Devkit:
-    # "Disparity maps are saved as uint16 PNG images, which can be opened with
-    # either MATLAB or libpng++. A 0 value indicates an invalid pixel (ie, no
-    # ground truth exists, or the estimation algorithm didn't produce an estimate
-    # for that pixel). Otherwise, the disparity for a pixel can be computed by
-    # converting the uint16 value to float and dividing it by 256.0"
-
-    img = imageio.imread(disp_path)
-    disp = img.astype('float32') / 256.
-    return disp
-
-def kittisf15_load_K_baseline(cam_to_cam_path):
-    import numpy as np
-    
-    K_line = None
-    T_00_line = None
-    T_01_line = None
-    with open(cam_to_cam_path, 'r') as f:
-        for l in f.readlines():
-            if 'P_rect_02' in l:
-                K_line = l
-            if 'T_02' in l:
-                T_00_line = l
-            if 'T_03' in l:
-                T_01_line = l
-    
-    assert K_line
-    params = K_line.split('P_rect_02: ')[-1]
-    params = [float(tok.strip()) for tok in params.split(' ') if tok]
-    K = np.array(params).reshape([3, 4])
-    K = K[:3, :3]
-    
-    assert T_00_line
-    assert T_01_line
-    params = T_00_line.split('T_02: ')[-1]
-    params = [float(tok.strip()) for tok in params.split(' ') if tok]
-    T_00 = np.array(params)
-    params = T_01_line.split('T_03: ')[-1]
-    params = [float(tok.strip()) for tok in params.split(' ') if tok]
-    T_01 = np.array(params)
-    baseline = np.linalg.norm(T_00 - T_01)
-    
-    return K, baseline
-
-def kittisf15_load_sflow(flow, K, baseline, disp0_path, disp1_path):
-    fx = K[0, 0]
-    
-    disp0 = kittisf15_load_disp(disp0_path)
-    disp0_valid = disp0[:, :] > 0
-    d0 = fx * baseline / (disp0 + 1e-5)
-    d0[~disp0_valid] = 0
-    
-    disp1 = kittisf15_load_disp(disp1_path)
-    disp1_valid = disp1[:, :] > 0
-    d1 = fx * baseline / (disp1 + 1e-5)
-    d1[~disp1_valid] = 0
-    
-    h, w = d1.shape[:2]
-    px_y = np.tile(np.arange(h)[:, np.newaxis], [1, w])
-    px_x = np.tile(np.arange(w)[np.newaxis, :], [h, 1])
-    pyx = np.concatenate([px_y[:,:,np.newaxis], px_x[:, :, np.newaxis]], axis=-1)
-    pyx = pyx.astype(np.float32)
-    
-    vud1 = np.dstack([pyx, d0]).reshape([-1, 3])
-    uvdviz_im1 = np.zeros((vud1.shape[0], 4))
-    uvdviz_im1[:, :3] = vud1[:, (1, 0, 2)]
-    uvdviz_im1[:, -1] = np.logical_and(
-                            (flow > 0).reshape([-1, 2])[:, 0], # Flow is valid
-                            (d0 > 0).reshape([-1]))            # Depth is valid
-
-    vu2 = (pyx + flow[:, :, (1, 0)]).reshape([-1, 2])
-    d2_valid = (d1 > 0).reshape([-1])
-    invalid = np.where(
-            (np.rint(vu2[:, 0]) < 0) | (np.rint(vu2[:, 0]) >= h) |
-            (np.rint(vu2[:, 1]) < 0) | (np.rint(vu2[:, 1]) >= w) |
-            (flow[:, :, 0] == 0).reshape([-1]) |
-            (~d2_valid))
-    j2 = np.rint(vu2[:, 0]).astype(np.int64)
-    i2 = np.rint(vu2[:, 1]).astype(np.int64)
-    j2[invalid] = 0
-    i2[invalid] = 0
-    d2_col = d1[j2, i2]
-    vud2 = np.hstack([vu2, d2_col[:, np.newaxis]])
-    
-    uvdviz_im2 = np.ones((vud1.shape[0], 4))
-    uvdviz_im2[:, :3] = vud2[:, (1, 0, 2)]
-    uvdviz_im2[invalid, -1] = 0
-    
-#     vudviz_im2[:, -1] = (vudviz_im2[:, 0] != -np.Inf)
-#     vudviz_im1[:, -1] = np.logical_and(vudviz_im1[:, -1], (vudviz_im1[:, 2] > 0))
-    
-    visible_either = ((uvdviz_im1[:, -1] == 1) | (uvdviz_im2[:, -1] == 1))
-    uvdviz_im1 = uvdviz_im1[visible_either]
-    uvdviz_im2 = uvdviz_im2[visible_either]
-#         xyz1 = uvd_to_xyzrgb(uvd1, fp.K)[:, :3]
-#         xyz2 = uvd_to_xyzrgb(uvd2, fp.K)[:, :3]     
-    
-    return uvdviz_im1, uvdviz_im2
-
-def kittisf15_create_fp(uri):
-  flow = kittisf15_load_flow(os.path.join(KITTI_SF15_DATA_ROOT, uri.extra['ksf15.flow_gt']))
-  K, baseline = kittisf15_load_K_baseline(os.path.join(KITTI_SF15_DATA_ROOT, uri.extra['ksf15.K']))
-  uvdviz_im1, uvdviz_im2 = kittisf15_load_sflow(
-                                  flow, K, baseline,
-                                  os.path.join(KITTI_SF15_DATA_ROOT, uri.extra['ksf15.disp0']),
-                                  os.path.join(KITTI_SF15_DATA_ROOT, uri.extra['ksf15.disp1']))
+def kittisf15_load_disp(disp_data):
+  import imageio
   
-  return OpticalFlowPair(
-              uri=uri,
-              dataset="KITTI Scene Flow 2015",
-              id1=uri.extra['ksf15.input'],
-              img1='file://' + os.path.join(KITTI_SF15_DATA_ROOT, uri.extra['ksf15.input']),
-              id2=uri.extra['ksf15.expected_out'],
-              img2='file://' + os.path.join(KITTI_SF15_DATA_ROOT, uri.extra['ksf15.expected_out']),
-              flow=flow,
+  # From KITTI SF Devkit:
+  # "Disparity maps are saved as uint16 PNG images, which can be opened with
+  # either MATLAB or libpng++. A 0 value indicates an invalid pixel (ie, no
+  # ground truth exists, or the estimation algorithm didn't produce an estimate
+  # for that pixel). Otherwise, the disparity for a pixel can be computed by
+  # converting the uint16 value to float and dividing it by 256.0"
+
+  img = imageio.imread(disp_data)
+  disp = img.astype('float32') / 256.
+  return disp
+
+def kittisf15_load_K_baseline(cam_to_cam_str):
+  import numpy as np
+  
+  K2_line = None
+  K3_line = None
+  T_00_line = None
+  T_01_line = None
+  for l in cam_to_cam_str.split('\n'):
+    if 'P_rect_02' in l:
+      K2_line = l
+    if 'P_rect_03' in l:
+      K3_line = l
+    if 'T_02' in l:
+      T_00_line = l
+    if 'T_03' in l:
+      T_01_line = l
+  
+  assert K2_line
+  params = K2_line.split('P_rect_02: ')[-1]
+  params = [float(tok.strip()) for tok in params.split(' ') if tok]
+  P_2 = np.array(params).reshape([3, 4])
+  K_2 = P_2[:3, :3]
+  
+  assert K3_line
+  params = K3_line.split('P_rect_03: ')[-1]
+  params = [float(tok.strip()) for tok in params.split(' ') if tok]
+  P_3 = np.array(params).reshape([3, 4])
+  K_3 = P_3[:3, :3]
+
+  assert T_00_line
+  assert T_01_line
+  params = T_00_line.split('T_02: ')[-1]
+  params = [float(tok.strip()) for tok in params.split(' ') if tok]
+  T_00 = np.array(params)
+  params = T_01_line.split('T_03: ')[-1]
+  params = [float(tok.strip()) for tok in params.split(' ') if tok]
+  T_01 = np.array(params)
+  baseline = np.linalg.norm(T_00 - T_01)
+  breakpoint()
+  return K_2, K_3, baseline, T_00, T_01, P_2, P_3
+
+def kittisf15_to_stereo_matches(disp, baseline, K_2):
+  fx = K_2[0, 0]
+  
+  disp_valid = disp[:, :] > 0
+  depth = fx * baseline / (disp + 1e-5)
+  depth[~disp_valid] = 0
+
+  h, w = disp.shape[:2]
+  px_y_2 = np.tile(np.arange(h)[:, np.newaxis], [1, w])
+  px_x_2 = np.tile(np.arange(w)[np.newaxis, :], [h, 1])
+  pyx_2 = np.concatenate([px_y_2[:,:,np.newaxis], px_x_2[:, :, np.newaxis]], axis=-1)
+  pyx_2 = pyx_2.astype(np.float32)
+
+  vud_2 = np.dstack([pyx_2, depth]).reshape([-1, 3])
+  uvd_2 = np.zeros((vud_2.shape[0], 3))
+  uvd_2[:, :3] = vud_2[:, (1, 0, 2)]
+  
+  uv_3 = uvd_2[:, :2].copy()
+  uv_3[:, 0] -= disp.reshape(-1)
+  uv_2_uv_3_depth = np.hstack([uvd_2[:, :2], uv_3, uvd_2[:, (-1,)]])
+  return uv_2_uv_3_depth
+
+# def kittisf15_load_sflow(flow, K, baseline, disp0_path, disp1_path):
+#   fx = K[0, 0]
+  
+#   disp0 = kittisf15_load_disp(disp0_path)
+#   disp0_valid = disp0[:, :] > 0
+#   d0 = fx * baseline / (disp0 + 1e-5)
+#   d0[~disp0_valid] = 0
+  
+#   disp1 = kittisf15_load_disp(disp1_path)
+#   disp1_valid = disp1[:, :] > 0
+#   d1 = fx * baseline / (disp1 + 1e-5)
+#   d1[~disp1_valid] = 0
+  
+#   h, w = d1.shape[:2]
+#   px_y = np.tile(np.arange(h)[:, np.newaxis], [1, w])
+#   px_x = np.tile(np.arange(w)[np.newaxis, :], [h, 1])
+#   pyx = np.concatenate([px_y[:,:,np.newaxis], px_x[:, :, np.newaxis]], axis=-1)
+#   pyx = pyx.astype(np.float32)
+  
+#   vud1 = np.dstack([pyx, d0]).reshape([-1, 3])
+#   uvdviz_im1 = np.zeros((vud1.shape[0], 4))
+#   uvdviz_im1[:, :3] = vud1[:, (1, 0, 2)]
+#   uvdviz_im1[:, -1] = np.logical_and(
+#               (flow > 0).reshape([-1, 2])[:, 0], # Flow is valid
+#               (d0 > 0).reshape([-1]))      # Depth is valid
+
+#   vu2 = (pyx + flow[:, :, (1, 0)]).reshape([-1, 2])
+#   d2_valid = (d1 > 0).reshape([-1])
+#   invalid = np.where(
+#       (np.rint(vu2[:, 0]) < 0) | (np.rint(vu2[:, 0]) >= h) |
+#       (np.rint(vu2[:, 1]) < 0) | (np.rint(vu2[:, 1]) >= w) |
+#       (flow[:, :, 0] == 0).reshape([-1]) |
+#       (~d2_valid))
+#   j2 = np.rint(vu2[:, 0]).astype(np.int64)
+#   i2 = np.rint(vu2[:, 1]).astype(np.int64)
+#   j2[invalid] = 0
+#   i2[invalid] = 0
+#   d2_col = d1[j2, i2]
+#   vud2 = np.hstack([vu2, d2_col[:, np.newaxis]])
+  
+#   uvdviz_im2 = np.ones((vud1.shape[0], 4))
+#   uvdviz_im2[:, :3] = vud2[:, (1, 0, 2)]
+#   uvdviz_im2[invalid, -1] = 0
+  
+# #   vudviz_im2[:, -1] = (vudviz_im2[:, 0] != -np.Inf)
+# #   vudviz_im1[:, -1] = np.logical_and(vudviz_im1[:, -1], (vudviz_im1[:, 2] > 0))
+  
+#   visible_either = ((uvdviz_im1[:, -1] == 1) | (uvdviz_im2[:, -1] == 1))
+#   uvdviz_im1 = uvdviz_im1[visible_either]
+#   uvdviz_im2 = uvdviz_im2[visible_either]
+# #     xyz1 = uvd_to_xyzrgb(uvd1, fp.K)[:, :3]
+# #     xyz2 = uvd_to_xyzrgb(uvd2, fp.K)[:, :3]   
+  
+#   return uvdviz_im1, uvdviz_im2
+
+# def kittisf15_create_fp(uri):
+#   flow = kittisf15_load_flow(os.path.join(KITTI_SF15_DATA_ROOT, uri.extra['ksf15.flow_gt']))
+#   K, baseline = kittisf15_load_K_baseline(os.path.join(KITTI_SF15_DATA_ROOT, uri.extra['ksf15.K']))
+#   uvdviz_im1, uvdviz_im2 = kittisf15_load_sflow(
+#                                   flow, K, baseline,
+#                                   os.path.join(KITTI_SF15_DATA_ROOT, uri.extra['ksf15.disp0']),
+#                                   os.path.join(KITTI_SF15_DATA_ROOT, uri.extra['ksf15.disp1']))
+  
+#   return OpticalFlowPair(
+#               uri=uri,
+#               dataset="KITTI Scene Flow 2015",
+#               id1=uri.extra['ksf15.input'],
+#               img1='file://' + os.path.join(KITTI_SF15_DATA_ROOT, uri.extra['ksf15.input']),
+#               id2=uri.extra['ksf15.expected_out'],
+#               img2='file://' + os.path.join(KITTI_SF15_DATA_ROOT, uri.extra['ksf15.expected_out']),
+#               flow=flow,
       
-              K=K,
-              uvdviz_im1=uvdviz_im1,
-              uvdviz_im2=uvdviz_im2)
+#               K=K,
+#               uvdviz_im1=uvdviz_im1,
+#               uvdviz_im2=uvdviz_im2)
 
 
 
--- test/datasets/test_kitti_sf.py
+++ test/datasets/test_kitti_sf.py
@@ -24,10 +24,65 @@ def test_kitti_sf_stereo_3d_viz():
   outdir = testutil.test_tempdir('test_kitti_sf_stereo_3d_viz')
 
   for frame in kitti_sf.Fixtures.STEREO_TEST_FRAMES:
-    assert False, frame
+    
+    disp_path = base_dir / f'training/disp_occ_0/{frame}.png'
+    disp = kitti_sf.kittisf15_load_disp(open(disp_path, 'rb'))
+
+    cam_to_cam_path = (
+      base_dir / f'training/calib_cam_to_cam/{frame.replace("_10", "")}.txt')
+    
+    K_2, K_3, baseline, T_00, T_01, P_2, P_3 = kitti_sf.kittisf15_load_K_baseline(open(cam_to_cam_path, 'r').read())
+
+    uv_2_uv_3_depth = kitti_sf.kittisf15_to_stereo_matches(disp, baseline, K_2)
+
+    import trimesh
+    import numpy as np
+    vs = uv_2_uv_3_depth[:, (0, 1, -1)]
+    vs = vs[vs[:,-1] > 0]
+    f_x = K_2[0, 0]
+    f_y = K_2[1, 1]
+    c_x = K_2[0, 2]
+    c_y = K_2[1, 2]
+    # breakpoint()
+    uvd2_x = (vs[:, 0] - c_x) / f_x
+    uvd2_y = (vs[:, 1] - c_y) / f_y
+    uvd2_z = np.ones_like(uvd2_y)
+    uvd2xyz = np.hstack([uvd2_x[:, None], uvd2_y[:, None], uvd2_z[:, None]])
+    uvd2xyz *= vs[:, (-1,)]
+    pc_tmesh_uvd = trimesh.points.PointCloud(vertices=uvd2xyz, colors=np.zeros_like(uvd2xyz))
+    scene = trimesh.Scene()
+    scene.add_geometry(pc_tmesh_uvd)
+    b = trimesh.exchange.gltf.export_glb(scene)
+    with open('/opt/psegs/debug.glb', 'wb') as f:
+      f.write(b)
+    
+    import cv2
+    import numpy as np
+    # P_2 = np.eye(3, 4)
+    # P_2[:3, :3] = K_2
+    # P_2[:, 3] = T_00
+    # P_3 = np.eye(3, 4)
+    # P_3[:3, :3] = K_3
+    # P_3[:, 3] = T_01
+    uv_2 = uv_2_uv_3_depth[:, 0:2][uv_2_uv_3_depth[:, -1] > 0]
+    uv_3 = uv_2_uv_3_depth[:, 2:4][uv_2_uv_3_depth[:, -1] > 0]
+    xyzh = cv2.triangulatePoints(P_2, P_3, uv_2.T, uv_3.T)
+    xyz = xyzh.T.copy()
+    xyz = xyz[:, :3] / xyz[:, (-1,)]
+    # xyz = xyz[:, :3]
+    # xyz = cv2.convertPointsFromHomogeneous(xyzh.T)
+    
+    pc_tmesh_xyz = trimesh.points.PointCloud(vertices=xyz.squeeze(), colors=np.zeros_like(xyz))
+    scene = trimesh.Scene()
+    scene.add_geometry(pc_tmesh_xyz)
+    b = trimesh.exchange.gltf.export_glb(scene)
+    with open('/opt/psegs/debug.xyz.glb', 'wb') as f:
+      f.write(b)
+    breakpoint()
+    assert False
+
 
     """
-     * load disp, k and baseline
      * compute uvd viz
      * save as a point cloud, check binary as well as viz / GLTF
 
